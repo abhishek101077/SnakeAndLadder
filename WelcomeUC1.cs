ee8ab12c1da969311d287ca0a6b1752e97a9d862
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-16 12:31:46 +0530

    The first file

---
+++ WelcomeUC1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_Ladder
+{
+    public class WelcomeUC1
+    { 
+        public static void solution()
+        {
+            Console.WriteLine("Hello welcome in snake and ladder");
+            int start = 0;
+            Console.WriteLine("The begining of game is:" + start);
+
+        }
+    }
+}
