4d64792ee100c8229298a662b2a8aeed3f612f1c
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-16 13:07:02 +0530

    The NO play problem

---
+++ OptionUC3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_Ladder
+{
+    internal class OptionUC3
+    {
+        public static void solution()
+        { Random rnd = new Random();
+            int diceroll = rnd.Next(3);
+
+            switch (diceroll)
+            {
+                case 1:
+                    Console.WriteLine("Ladder");
+                    break;
+                case 2:
+                    Console.WriteLine("Snake");
+                    break;
+                case 0:
+                    Console.WriteLine("No Play");
+                    break;
+            }
+             
+            }
+
+        }
+    }
