43c606c8d3d3c35e5760430ef4aeaef0994b05ea
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-16 12:59:36 +0530

    The dice problem

---
+++ DiceUC2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_Ladder
+{
+    internal class DiceUC2
+    {
+        public static void solution()
+        {
+            Random random = new Random();
+            int diceOutput = random.Next(7);
+            int position = 0;
+            Console.WriteLine("The outcome of number :" +diceOutput);
+            Console.ReadLine();
+        }
+
+
+        }
+    }
